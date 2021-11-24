65872fac4b818f206859d98b1b821abf72fc9cb5
youssefelhaimer8 <youssefelhaimer8@gmail.com>
2021-11-24 20:19:29 +0100

    function printf

--- ft_printf.c
+++ ft_printf.c
@@ -6,56 +6,62 @@
 /*   By: yoelhaim <yoelhaim@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/22 18:04:09 by yoelhaim          #+#    #+#             */
-/*   Updated: 2021/11/24 12:49:56 by yoelhaim         ###   ########.fr       */
+/*   Updated: 2021/11/24 19:31:50 by yoelhaim         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "ft_printf.h"
+#include "include/ft_printf.h"
 
-static void	printPrintf(char  format,const void *value, int *len)
+static void printPrintf(char format, const void *value, int *len)
 {
 	if (format == 's')
-		*len += ft_putstr((char *) value);
+		*len += ft_putstr((char *)value);
 	else if (format == 'd' || format == 'i')
-		*len += ft_putnbr((int) value);
+		*len += ft_putnbr((int)value);
 	else if (format == 'c')
-		ft_putchar((char)value);
+		*len += ft_putchar((char)value);
 	else if (format == 'p')
 		*len += pointeur_print((void *)value);
-	else if(format == '%')
-		ft_putchar('%');
-
+	else if (format == 'u')
+		*len += ft_putnbr_unsign((unsigned)value);
+	else if (format == 'x')
+		*len += ft_putnbr_base((unsigned int)value, "0123456789abcdef");
+	else if (format == 'X')
+		*len += ft_putnbr_base((unsigned int)value, "0123456789ABCDEF");
+	else if (format == '%')
+		*len += ft_putchar('%');
 }
 
 int ft_printf(const char *frmt, ...)
 {
-	va_list	lst;
-	void	*to_next;
-	int		lenght;
+	va_list lst;
+	void *to_next;
+	int lenght;
 
 	lenght = 0;
 	va_start(lst, frmt);
-	while(*frmt)
+	while (*frmt)
 	{
-		if(*frmt == '%')
+		if (*frmt == '%')
 		{
 			frmt++;
-			if(*frmt != '%')
-				 to_next = va_arg(lst, void *);
-            printPrintf(*frmt, to_next, &lenght);
-
+			if (*frmt != '%')
+				to_next = va_arg(lst, void *);
+			printPrintf(*frmt, to_next, &lenght);
 		}
 		else
-           lenght = ft_putchar(*frmt);
-        frmt++;
+			lenght += ft_putchar(*frmt);
+		frmt++;
 	}
 	va_end(lst);
-	return(lenght);
+	return (lenght);
 }
-#include <stdio.h>
-int main()
-{
-	 ft_printf("string => %s | dicimle => %d | char=> %c | pointeur => %p \n","abcd", 1234, 'a',"abc");
-	  printf("string => %s | dicimle => %d | char=> %c | pointeur => %p \n","abcd", 1234, 'a',"abc");
+// #include <stdio.h>
+// int main()
+// {
+// 	// ft_printf("string => %.2s | dicimle => %d | char=> %c | pointeur => %p | hex => %x | HEX => %X \n","abcd", 1234, 'a',"abc", 0xC0, 0xC0);
+// 	// printf("string => %.2s | dicimle => %d | char=> %c | pointeur => %p | hex => %x | HEX => %X \n","abcd", 1234, 'a',"abc", 0xC0, 0xC0);
 
-}
+// int c = ft_printf("%s\n", "ahmed");
+// ft_printf("%d", c);
+// }
