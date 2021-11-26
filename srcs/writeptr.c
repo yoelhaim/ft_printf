8ea2eda96a5dae868bdb29d745ee2716eef654b4
youssefelhaimer8 <youssefelhaimer8@gmail.com>
2021-11-26 13:58:58 +0100

    norm write ptr

--- srcs/writeptr.c
+++ srcs/writeptr.c
@@ -5,18 +5,18 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: yoelhaim <yoelhaim@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/11/24 12:45:23 by yoelhaim          #+#    #+#             */
-/*   Updated: 2021/11/24 14:08:15 by yoelhaim         ###   ########.fr       */
+/*   Created: 2021/11/26 13:51:16 by yoelhaim          #+#    #+#             */
+/*   Updated: 2021/11/26 13:51:51 by yoelhaim         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/ft_printf.h"
 
-int    pointeur_print(void *p)
+int	pointeur_print(void *p)
 {
-    unsigned long    x;
+	unsigned long	x;
 
-    x = (unsigned long)p;
-    ft_putstr("0x");
-    return (ft_putnbr_base_ptr(x, "0123456789abcdef") + 2);
+	x = (unsigned long)p;
+	ft_putstr("0x");
+	return (ft_putnbr_base_ptr(x, "0123456789abcdef") + 2);
 }
