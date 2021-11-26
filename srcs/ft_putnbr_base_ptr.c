80b4f9c9a3f4f6ce9180e2287b0963de2fa7398a
youssefelhaimer8 <youssefelhaimer8@gmail.com>
2021-11-26 14:00:24 +0100

    romove and create base

--- srcs/ft_putnbr_base_ptr.c
+++ srcs/ft_putnbr_base_ptr.c
@@ -5,41 +5,41 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: yoelhaim <yoelhaim@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/11/24 12:44:53 by yoelhaim          #+#    #+#             */
-/*   Updated: 2021/11/24 18:57:05 by yoelhaim         ###   ########.fr       */
+/*   Created: 2021/11/26 13:53:41 by yoelhaim          #+#    #+#             */
+/*   Updated: 2021/11/26 13:56:03 by yoelhaim         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/ft_printf.h"
 
-static int    calculate_length_ptr(unsigned long nb, char *base)
+static int	calculate_length_ptr(unsigned long nb, char *base)
 {
-    int        i;
+	int	i;
 
-    i = 0;
-    if (nb == 0)
-        i++;
-    while (nb > 0)
-    {
-        i++;
-        nb /= ft_strlen(base);
-    }
-    return (i);
+	i = 0;
+	if (nb == 0)
+		i++;
+	while (nb > 0)
+	{
+		i++;
+		nb /= ft_strlen(base);
+	}
+	return (i);
 }
 
-int    ft_putnbr_base_ptr(unsigned long nb, char *base)
+int	ft_putnbr_base_ptr(unsigned long nb, char *base)
 {
-    int        len;
+	int	len;
 
-    len = 0;
-    if (nb <= (unsigned long)ft_strlen(base) - 1)
-    {
-        ft_putchar(base[nb]);
-    }
-    else
-    {
-        ft_putnbr_base(nb / ft_strlen(base), base);
-        ft_putnbr_base(nb % ft_strlen(base), base);
-    }
-    return (len + calculate_length_ptr(nb, base));
+	len = 0;
+	if (nb <= (unsigned long)ft_strlen(base) - 1)
+	{
+		ft_putchar(base[nb]);
+	}
+	else
+	{
+		ft_putnbr_base(nb / ft_strlen(base), base);
+		ft_putnbr_base(nb % ft_strlen(base), base);
+	}
+	return (len + calculate_length_ptr(nb, base));
 }
