70989eaceb811792a338550f7b8448ab56c38d10
youssefelhaimer8 <youssefelhaimer8@gmail.com>
2021-11-26 13:59:07 +0100

    norm strlen

--- srcs/ft_strlen.c
+++ srcs/ft_strlen.c
@@ -6,17 +6,18 @@
 /*   By: yoelhaim <yoelhaim@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/24 12:57:06 by yoelhaim          #+#    #+#             */
-/*   Updated: 2021/11/24 14:08:08 by yoelhaim         ###   ########.fr       */
+/*   Updated: 2021/11/26 13:47:12 by yoelhaim         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/ft_printf.h"
 
-int ft_strlen(char *s)
+int	ft_strlen(char *s)
 {
-	int i;
+	int	i;
+
 	i = 0;
-	while(s[i])
+	while (s[i])
 		i++;
-	return(i);
+	return (i);
 }
