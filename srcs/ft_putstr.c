fcfde949ee058fc99e50c2a06d2e72b972491bcc
youssefelhaimer8 <youssefelhaimer8@gmail.com>
2021-11-26 13:59:40 +0100

    norm putstr

--- srcs/ft_putstr.c
+++ srcs/ft_putstr.c
@@ -5,21 +5,21 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: yoelhaim <yoelhaim@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/11/24 12:45:06 by yoelhaim          #+#    #+#             */
-/*   Updated: 2021/11/24 18:56:38 by yoelhaim         ###   ########.fr       */
+/*   Created: 2021/11/26 13:45:27 by yoelhaim          #+#    #+#             */
+/*   Updated: 2021/11/26 13:46:32 by yoelhaim         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/ft_printf.h"
 
-int    ft_putstr(char *s)
+int	ft_putstr(char *s)
 {
-    char    *ptr;
+	char	*ptr;
 
-    if (!s)
-        return (ft_putstr("(null)"));
-    ptr = s;
-    while (*s)
-        ft_putchar(*(s++));
-    return (s - ptr);
+	if (!s)
+		return (ft_putstr("(null)"));
+	ptr = s;
+	while (*s)
+		ft_putchar(*(s++));
+	return (s - ptr);
 }
