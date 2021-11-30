d133f3391e69ff651adf47ee2c98c3b36d987a5d
youssefelhaimer8 <youssefelhaimer8@gmail.com>
2021-11-30 19:04:45 +0100

    update strlen to 16

--- srcs/ft_putnbr_base_ptr.c
+++ srcs/ft_putnbr_base_ptr.c
@@ -6,13 +6,13 @@
 /*   By: yoelhaim <yoelhaim@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/26 13:53:41 by yoelhaim          #+#    #+#             */
-/*   Updated: 2021/11/26 13:56:03 by yoelhaim         ###   ########.fr       */
+/*   Updated: 2021/11/30 13:30:38 by yoelhaim         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/ft_printf.h"
 
-static int	calculate_length_ptr(unsigned long nb, char *base)
+static int	calculate_length_ptr(unsigned long nb)
 {
 	int	i;
 
@@ -22,24 +22,21 @@ static int	calculate_length_ptr(unsigned long nb, char *base)
 	while (nb > 0)
 	{
 		i++;
-		nb /= ft_strlen(base);
+		nb /= 16;
 	}
 	return (i);
 }
 
 int	ft_putnbr_base_ptr(unsigned long nb, char *base)
 {
-	int	len;
-
-	len = 0;
 	if (nb <= (unsigned long)ft_strlen(base) - 1)
 	{
 		ft_putchar(base[nb]);
 	}
 	else
 	{
-		ft_putnbr_base(nb / ft_strlen(base), base);
-		ft_putnbr_base(nb % ft_strlen(base), base);
+		ft_putnbr_base(nb / 16, base);
+		ft_putnbr_base(nb % 16, base);
 	}
-	return (len + calculate_length_ptr(nb, base));
+	return (calculate_length_ptr(nb));
 }
